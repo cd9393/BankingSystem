5753f2226b3523ba9fd2200c52cc3bb45909963e
Craig Dunlop <craigdun9393@gmail.com>
2022-02-20 18:27:16 +0000

    implemeneted Luhn algorithm to pass stageTwo

--- src/BankingSystem/Bank/AccountCreator.java
+++ src/BankingSystem/Bank/AccountCreator.java
@@ -1,11 +1,16 @@
 package BankingSystem.Bank;
 
+
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Random;
 
 public class AccountCreator {
 
-    public AccountCreator(){
+    private Map<Long,String> generatedAccount;
 
+    public AccountCreator(){
+        this.generatedAccount = new HashMap<>();
     }
 
     public Account generateAccount() {
@@ -18,18 +23,27 @@ public class AccountCreator {
     private String generateCardNo(){
         StringBuilder cardNo = new StringBuilder();
         cardNo.append("400000");
-        cardNo.append(generateAccountNumber());
-        cardNo.append(generateCheckSum());
+        cardNo.append(createUniqueAccount());
+        int checkSum = generateCheckSum(cardNo.toString());
+        cardNo.append(checkSum);
         return cardNo.toString();
     }
 
     private String generatePIN() {
         StringBuilder pin = new StringBuilder();
-        pin.append(generateCheckSum());
-        pin.append(generateCheckSum());
-        pin.append(generateCheckSum());
-        pin.append(generateCheckSum());
+        for (int i = 0; i < 4; i++) {
+            pin.append(generateDigit());
+        }
         return pin.toString();
+    }
+
+    private long createUniqueAccount(){
+        long accountNumber = generateAccountNumber();
+        while(generatedAccount.containsKey(accountNumber)) {
+            accountNumber = generateAccountNumber();
+        }
+        generatedAccount.put(accountNumber,"added");
+        return accountNumber;
     }
 
     private Long generateAccountNumber() {
@@ -39,9 +53,32 @@ public class AccountCreator {
         return accountNumber;
     }
 
-    private int generateCheckSum() {
+    private int generateCheckSum(String cardNo) {
+        int control = findControlNumber(cardNo);
+        System.out.println("control = " + control);
+        return (10 - control % 10)  % 10;
+    }
+
+    private int generateDigit() {
         int max = 10;
         Random random = new Random();
         return random.nextInt(max);
     }
+
+    private int findControlNumber(String cardNo){
+        int sum = 0;
+        int digit;
+        for(int i = 0; i < cardNo.length(); i++) {
+            digit = Character.getNumericValue(cardNo.charAt(i));
+            if (i % 2 == 0) {
+                digit *= 2;
+                if(digit > 9) {
+                    digit -= 9;
+                }
+            }
+            sum += digit;
+        }
+        return sum;
+    }
 }
+
